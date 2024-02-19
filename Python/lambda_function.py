a53ef10ae1a336b31a6a2c965abc85166eb7fa57
Debadatta <debadatta30@gmail.com>
2024-02-19 21:23:55 +0000

    Adding the Cloudformation template and Lambda code

---
+++ Python/lambda_function.py
@@ -0,0 +1,64 @@
+import json
+import boto3
+import logging
+import os
+from jinja2 import Template
+
+# Set up the  logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Env variables from  CFN 
+bucket_name = os.environ['SOURCEBUCKET']
+key = os.environ['SOURCEFILE']
+
+s3_client = boto3.client('s3')
+bedrock_runtime = boto3.client('bedrock-runtime', 'us-east-1')
+
+def lambda_handler(event, context):
+    #print("Received event: " + json.dumps(event, indent=2))
+    try:
+        review_content = ""
+        response = s3_client.get_object(Bucket=bucket_name, Key="review.txt")
+        review_content = response['Body'].read().decode('utf-8')
+        prompt_content = ""
+        response = s3_client.get_object(Bucket=bucket_name, Key=key)
+        prompt_content = response['Body'].read().decode('utf-8')
+        data = {
+            'transcript' : review_content
+        }
+        template = Template(prompt_content)
+        prompt = template.render(data)
+        print(prompt)
+
+        config = {
+            "modelId": "amazon.titan-text-express-v1",
+            "contentType": "application/json",
+            "accept": "*/*",
+            "body": json.dumps(
+                {
+                    "inputText": prompt,
+                    "textGenerationConfig": {
+                        "maxTokenCount": 500,
+                        "temperature": 0,
+                        "topP": 0.9
+                    }
+                }
+            )
+        }
+        response = bedrock_runtime.invoke_model(**config)
+        summarize = json.loads(response.get('body').read()).get('results')[0].get('outputText')   
+        Keyresult = key + 'results.txt'
+        s3_client.put_object(
+            Bucket=bucket_name,
+            Key=Keyresult,
+            Body=summarize,
+            ContentType='text/plain'
+        ) 
+        logger.info(response)
+    except Exception as e:
+        #print(e)
+        logger.info(e)
+        raise e
+        
+        
